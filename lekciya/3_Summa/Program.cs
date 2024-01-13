8110afc49509d1ba58179324db334402a0116601
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-13 18:44:36 +0700

    Сумма; 3 задача; 1 лекция

---
+++ lekciya/3_Summa/Program.cs
@@ -0,0 +1,17 @@
+﻿// Сумма 2 чисел
+
+double NumberA = 12;
+double NumberB = 5;
+Console.WriteLine(NumberA + NumberB);
+
+
+/*
+Console.Write(" Введите первое число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите второе число: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+int Summa = NumberA + NumberB;
+Console.Write(" Сумма = ");
+Console.WriteLine(Summa);
+*/
