a16afe256cc84ecd3e3118746ec8d1e46717ad00
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-13 18:06:56 +0700

    привет пользователь! 1 лекция

--- lekciya/2_privet_polzovatel/Program.cs
+++ lekciya/2_privet_polzovatel/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Привет пользователь!
+
+Console.WriteLine(" Введите ваше Имя: ");// Выводим просьбу ввести имя
+string username = Console.ReadLine();// Считываем данные с консоли до нажатия enter
+Console.Write(" Привет, ");// Выводим фразу Привет с запятой и пробелом
+Console.WriteLine(username);// Выводим введеное ранее имя
+
+/*
+Console.WriteLine(" Введите ваше Имя");
+string username = Console.ReadLine();
+Console.WriteLine(" Привет, ", username);
+*/
