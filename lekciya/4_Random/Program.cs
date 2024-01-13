78b539550b36d68d0424eaaec02ca9b31740b19c
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-13 18:55:06 +0700

    Рандомные числа; 3 задача; 1 лекция

---
+++ lekciya/4_Random/Program.cs
@@ -0,0 +1,9 @@
+﻿// Сумма рандомных чисел
+
+int NumberA = new Random().Next(1,10); // Генерируем случайное число от 1 до 9
+Console.WriteLine(NumberA); // выводим полученое число
+int NumberB = new Random().Next(1,10); // генерируем второе случайное число от 1 до 9
+Console.WriteLine(NumberB); // Выводим второе число
+int result = NumberA + NumberB; // Складываем числа
+Console.Write(" Сумма = "); // Выводим Сумма =
+Console.WriteLine(result); // Выводим значение параметра result
