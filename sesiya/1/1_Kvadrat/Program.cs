bab6860ed37fc2adaea1070dba75d247d8f4eea6
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-15 21:41:21 +0700

    Квадрат; 1 сессия

---
+++ sesiya/1/1_Kvadrat/Program.cs
@@ -0,0 +1,16 @@
+﻿// возведение в квадрат
+
+using System; // подключаем библиотеку System
+using static System.Console; // говорим использовать консоль по умолчанию
+
+Write(" Введите число: ");
+int number = int.Parse(ReadLine()); //считываем консоль и переводим строку в число
+int sqr = number * number;
+WriteLine($" Результат = {sqr} "); // $ позволяет выводить смешаный вывод (Форматирование строки)
+// WriteLine($" Результат = " + sqr);
+
+/*
+Console.Write(" Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A * A);
+*/
