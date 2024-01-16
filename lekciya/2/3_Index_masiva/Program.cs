7d4b00134c80dc30fbd219530b974dff88f36675
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-16 23:01:13 +0700

    2 лекция

---
+++ lekciya/2/3_Index_masiva/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using static System.Console;
+
+int[] array = { 1, 12, 31, 4, 15, 16, 17, 18 };
+
+int n = array.Length; // Length == длина массива, array == массив
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
---
+++ lekciya/2/4_Array/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using static System.Console;
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Write(col[position]);
+        Write(" ");
+        position++;
+    }
+}
+
+int Idex0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+WriteLine();
+
+int pos = Idex0f(array, 444);
+Write(pos);
