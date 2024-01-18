4378a10c46acb78717af1d76d4b8fef82b7ac277
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-18 23:50:41 +0700

    3 лекция

---
+++ lekciya/3/1_zadacha/Program.cs
@@ -0,0 +1,90 @@
+﻿// 
+
+using System;
+using static System.Console;
+Clear();
+
+/*
+// Метод первый 
+void Method1() // void не возвращает значений
+{
+    WriteLine(" Автор ... ");
+
+}
+Method1();
+*/
+
+/*
+// Метод 2
+void Method2(string msg)
+{
+    WriteLine(msg);
+}
+Method2(" Текст сообщения ");
+*/
+/*
+void Method21(string msg, int count)
+{
+    int i =0;
+    while(i< count)
+    {
+        WriteLine(msg);
+        i++;
+    }
+}
+//Method21(" Текст ", 4);
+Method21(count: 4, msg: " Новый текст ");
+*/
+
+/*
+// Метод 3
+int Method3()
+{
+return DateTime.Now.Year;
+}
+
+int year = Method3();
+WriteLine(year);
+*/
+
+/*
+// Метод 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i<count)
+    {
+        result= result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf ");
+Write(res);
+*/
+
+/*
+string Method41(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method41(10, "asdf ");
+Write(res);
+*/
+
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        WriteLine($" {i} x {j} = {i*j}");
+    }
+    WriteLine(" ");
+}
+*/
---
+++ lekciya/3/2_zadacha/Program.cs
@@ -0,0 +1,32 @@
+﻿// дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+
+using System;
+using  static System.Console;
+Clear();
+
+string text = " - Я думаю, - сказал князь, улыбаясь, - что, ежели бы вас послали всместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чай?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i<length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i] }";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+WriteLine(newText);
+WriteLine();
+newText = Replace(newText, 'к', 'К');
+WriteLine(newText);
+WriteLine();
+newText = Replace(newText, 'С', 'с');
+WriteLine(newText);
