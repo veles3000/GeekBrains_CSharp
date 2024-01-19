8ce6ae1829ac4300fb8f26c1a06995f7513ca7ec
veles3000 <103363629+veles3000@users.noreply.github.com>
2024-01-19 14:43:26 +0700

    Домашняя работа 2 семинар

---
+++ sesiya/2/DZ/1_Zadacha/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using System;
+using static System.Console;
+Clear();
+
+/*
+// НОВОЕ РЕШЕНИЕ
+Write(" Введите первое число: ");
+int num = Convert.ToInt32(ReadLine());
+
+string numS = Convert.ToString(num);
+Write(numS[1]);
+*/
+
+
+// СТАРОЕ РЕШЕНИЕ
+int a1;
+Console.Write("Введите трех значное число: ");
+a1 = Convert.ToInt32(Console.ReadLine());
+if (a1 > 99)
+{
+    if (a1 < 1000)
+    {
+        a1 = a1 / 10;
+        int result = a1 % 10;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine(" Число не соответствует критерию ");
+    }
+}
+else
+{
+    if (a1 < -99)
+    {
+        if (a1 > -1000)
+        {
+            a1 = a1 / 10;
+            int result = a1 % 10;
+            Console.WriteLine(-result);
+        }
+        else
+        {
+            Console.WriteLine(" Число не соответствует критерию ");
+        }
+    }
+    else
+    {
+        Console.WriteLine(" Число не соответствует критерию ");
+    }
+}
---
+++ sesiya/2/DZ/2_Zadacha/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+using System;
+using static System.Console;
+Clear();
+
+/*
+// Новое решение
+Write(" Введите первое число: ");
+int num = Convert.ToInt32(ReadLine());
+
+string numS = Convert.ToString(num);
+Write(numS.Length > 2?numS[2]:$" В числе {num} нет третьего символа");
+*/
+
+
+// СТАРОЕ РЕШЕНИЕ
+int n;
+Console.Write(" Введите число: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(n>99?n.ToString()[2]:n<-99?n.ToString()[3]: " нет третьего элемента");
