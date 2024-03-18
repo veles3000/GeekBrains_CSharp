fd726fa9152176d0a75ff9fad032ac70f49ae34b
veles3000 <ya.velesov@gmail.com>
2024-03-18 23:33:52 +0700

    не менял

--- lekciya/1/1_Hello/obj/Debug/net8.0/1.AssemblyInfo.cs
+++ lekciya/1/1_Hello/obj/Debug/net8.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f50eff3fced48f59fb06e57fc32b3e975d81a2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d3c715a32d2f71fdc9a463ef042f205632edde")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
